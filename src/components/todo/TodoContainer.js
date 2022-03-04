eb247c4741ecc1327c47c946740842a1e0c408a5
imhjlov <30456236+imhjlov@users.noreply.github.com>
2022-03-05 03:34:56 +0900

    feat : todoCreate 컴포넌트 구현

--- src/components/todo/TodoContainer.js
+++ src/components/todo/TodoContainer.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useTodo } from 'components/todo/TodoService';
+import TodoCreate from 'components/todo/create/TodoCreate';
+import Count from 'components/todo/Count';
+import TodoList from 'components/todo/list/TodoList';
+import { TODO_STATUS } from 'utils/constants';
 
-const TodoContainer = (props) => {
-  return <TodoContainerBlock>{props.children}</TodoContainerBlock>;
+const TodoContainer = ({ showTodo }) => {
+  const { todoState, nextIdState, incrementNextId, toggleTodo, removeTodo, createTodo } = useTodo();
+  return (
+    <TodoContainerBlock>
+      <TodoCreate nextId={nextIdState} createTodo={createTodo} incrementNextId={incrementNextId} />
+      <Count todos={todoState} />
+    </TodoContainerBlock>
+  );
 };
 
 export default TodoContainer;
 
 const TodoContainerBlock = styled.div`
-  border: 1px solid red;
+  width: 100%;
+  background-color: #f3f3f3;
+  border-radius: 10px;
+  box-shadow: 0 4px 4px #999999;
+  padding: 30px 40px;
+  margin-bottom: 52px;
 `;
--- src/components/todo/create/TodoCreate.js
+++ src/components/todo/create/TodoCreate.js
@@ -1,21 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
-const TodoCreate = () => {
+const TodoCreate = ({ nextId, createTodo, incrementNextId }) => {
+  const [value, setValue] = useState('');
+
+  const handleChange = (e) => setValue(e.target.value);
+
+  const handleCreate = (e) => {
+    if (value === '') {
+      return;
+    }
+    createTodo({
+      id: nextId,
+      text: value,
+      done: false,
+    });
+    incrementNextId();
+    setValue('');
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') handleCreate();
+  };
+
   return (
     <CreateContainer>
-      <input />
-      <CreateBtn />
+      <Input
+        autoFocus
+        placeholder="할 일을 등록 하세요."
+        onChange={handleChange}
+        value={value}
+        onKeyPress={handleKeyPress}
+      />
+      <CreateBtn onClick={handleCreate}>추가</CreateBtn>
     </CreateContainer>
   );
 };
 
 export default TodoCreate;
 
-const CreateContainer = styled.span`
-  border: 1px solid red;
+const CreateContainer = styled.div`
+  width: 100%;
+  height: 55px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid #999999;
+  border-radius: 10px;
+  background-color: #fff;
+  padding: 0 34px;
+`;
+
+const Input = styled.input`
+  outline: none;
+  font-size: 21px;
+  &::placeholder {
+    color: #999999;
+    font-size: 18px;
+  }
 `;
 
 const CreateBtn = styled.button`
-  border: 1px solid red;
+  background-color: #000000;
+  color: #ffffff;
+  border-radius: 10px;
+  padding: 6px 40px;
 `;
